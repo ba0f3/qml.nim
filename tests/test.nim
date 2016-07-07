edd4e7394b3184d878d1ddc86b2a1d121c0d55aa
Bruce Doan <rgv151@gmail.com>
2016-07-07 16:32:02 +0700

    test just works

--- tests/test.nim
+++ tests/test.nim
@@ -1,11 +1,16 @@
 import qml
 
 newGuiApplication()
+
 var
   engine = newEngine(nil)
-  component = newComponent(engine, nil)
+  comp = newComponent(engine, nil)
 
-component.componentLoadURL("test.qml", 8)
-
-var window = component.componentCreateWindow(nil)
-window.windowShow()
+comp.componentLoadURL("test.qml", 8)
+let message = componentErrorString(comp)
+if message != nil:
+   echo "Error: ", message
+var win = comp.componentCreateWindow(nil)
+windowShow(win)
+windowConnectHidden(win)
+applicationExec()
